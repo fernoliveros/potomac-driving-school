9d2342159bd9b6ffb9cecdc395e0895830509e2a
fernoliveros <fernoliveros@gmail.com>
2021-04-07 19:10:27 -0400

    call api gateway1

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -35,7 +36,7 @@ import { CardComponent } from './components/common/card/card.component';
     PostComponent,
     CardComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule],
+  imports: [BrowserModule, AppRoutingModule, ReactiveFormsModule, FormsModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- ui/src/app/components/pages/register/register.component.ts
+++ ui/src/app/components/pages/register/register.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { FormControl } from '@angular/forms';
+import { HttpClient } from '@angular/common/http';
+import * as aws4 from "aws4";
 
 @Component({
   selector: 'app-register',
@@ -10,12 +12,33 @@ import { FormControl } from '@angular/forms';
 export class RegisterComponent implements OnInit {
   public classSelect = new FormControl('');
 
-  constructor(private route: ActivatedRoute) {}
+  constructor(
+    private route: ActivatedRoute,
+    private http: HttpClient
+    ) {}
 
   ngOnInit(): void {
     this.route.queryParams.subscribe((qp: { cc: string }) => {
       this.classSelect.setValue(qp.cc);
       console.log(this.classSelect.value);
     });
+  }
+
+  public submitRegistration(): void {
+    let request = {
+      host: '/qgsw8guhb1.execute-api.us-east-1.amazonaws.com',
+      method: 'GET',
+      url:  `https://qgsw8guhb1.execute-api.us-east-1.amazonaws.com/default/helloWorldLambda`,
+      path: '/default/helloWorldLambda'
+    }
+   
+     let signedRequest = aws4.sign(request, {
+       secretAccessKey: 'cP7pLprQkDaT8VoceJbaleqXOsZMaFXHqL2sE+21',
+       accessKeyId: 'AKIA4CFHWVLTHA4V3G7O',
+      //  secretAccessKey: AWS.config.credentials.secretAccessKey,
+      //  accessKeyId: AWS.config.credentials.accessKeyId,
+     });
+     delete signedRequest.headers['Host'];
+     this.http.get(signedRequest.url, signedRequest).subscribe(res => console.log(res), err => console.log(err));
   }
 }
